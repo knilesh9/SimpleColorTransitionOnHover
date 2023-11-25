f6e96d22ffbd8649a907ff3863b9f39f835644ff
itsmek <yyooggeesshh0011@gmail.com>
2023-11-25 19:47:34 +0530

    color ease in and out done

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 var rect = document.querySelector(".rectangle");
 
+rect.addEventListener("mousemove", function(details){
+
+    console.log(details.layerX);
+
+    if(details.layerX < rect.clientWidth/2 ){
+
+        var greenColor = gsap.utils.mapRange(0,rect.clientWidth/2,
+                    255, 0, details.layerX);
+
+        gsap.to(".rectangle", {
+            backgroundColor: `rgb(0, ${greenColor}, 0)`,
+            ease: Power4,
+        });
+
+    }else if(details.layerX > rect.clientWidth/2){
+        var blueColor = gsap.utils.mapRange(rect.clientWidth/2, rect.clientWidth,
+                    0, 255, details.layerX);
+
+        gsap.to(".rectangle", {
+            backgroundColor: `rgb(0, 0, ${blueColor})`,
+            ease: Power4,
+        });
+    }
+
+});
+
